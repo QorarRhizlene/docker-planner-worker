4d408ce89fb3910894cd8e11cd804692cbab93b5
QorarRhizlene <rhizlene.qorar0605@gmail.com>
2023-11-24 15:19:13 +0100

    Correction exercice 1

--- planner/connect.sh
+++ planner/connect.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 for var in "$@"
 do
-    curl -X POST localhost:3000/register  -H "Content-Type: application/json"  -d "{\"url\": \"http://localhost:$var\"}"
+    curl -X POST localhost:8080/register -H "Content-Type: application/json" -d "{\"url\": \"http://localhost:$var\"}"
 done
